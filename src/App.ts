046e150024b12db05e69dc280605d24069ab704f
Pedro Moreira <pedro.ms.moreira51@gmail.com>
2017-01-21 21:58:14 +0000

    Add another route. GetAll and GetOne implementation for Hero Routes.

--- src/App.ts
+++ src/App.ts
@@ -2,6 +2,7 @@ import * as path from 'path';
 import * as express from 'express';
 import * as logger  from 'morgan';
 import * as bodyParser from 'body-parser';
+import HeroRouter from './routes/HeroRouter';
 
 // Creates and configures an ExpressJS web server
 class App{
@@ -37,6 +38,7 @@ class App{
             });
         });
         this.express.use('/', router);
+        this.express.use('/api/v1/heroes', HeroRouter);
     }
 }
 
